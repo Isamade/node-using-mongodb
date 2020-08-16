13f1f3e3f2045223b9a44c7b0c5389e4b70869d2
Isamade <52768347+Isamade@users.noreply.github.com>
2020-08-16 23:58:54 +0100

    module3

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,15 @@
-import {addnewProduct} from '../controllers/controllers';
+import {addnewProduct,
+        getProducts,
+        getProductWithID
+} from '../controllers/controllers';
 
 const routes = (app) => {
     app.route('/products')
+        .get(getProducts)
+        .post(addnewProduct);
 
-    //Post endpoint
-    .post(addnewProduct);
+    app.route('/products/:ProductID')
+        .get(getProductWithID);
 }
 
 export default routes;
